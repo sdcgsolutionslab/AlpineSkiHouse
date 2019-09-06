3d979e95dc2d2eede2cce1d5b13126efbd4034f8
sdcgsolutionslab <sdcg.solutions.lab@hpe.com>
2019-09-06 16:21:05 +0200

    Add files via upload

---
+++ AlpineSkiHouse/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlpineSkiHouse.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Alpine Ski House is the premier ski hill in Northeast.";
+        }
+    }
+}
